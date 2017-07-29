4746bba0cb926d1303c0ff17023424cc5be71d4e
bryanrtboy <bryan@bryanleister.com>
2017-07-29 17:42:09 +0000

    Updated obj files

--- ClientMoviePlayer.py
+++ ClientMoviePlayer.py
@@ -3,6 +3,7 @@
 from pssh import SSHClient, ParallelSSHClient, utils
 import random
 import sys
+import time
 
 output = []
 hosts = ['client0', 'client1', 'client2','client3', 'client4']
@@ -13,6 +14,8 @@ ssh_clients = []
 #open a ssh connection for each host
 for host in hosts :
 	one_client = SSHClient(host)
+	print("Connecting to ", host)
+	time.sleep(1)
 	ssh_clients.append(one_client)
 	
 def open_movie(choice, clientID) :
@@ -25,11 +28,15 @@ def open_movie(choice, clientID) :
 	
 	
 def play_screensaver() :
-	cmds = ["~/dbuscontrol.sh stop", "omxplayer /mnt/usb/media/intro.mp4 --aspect-mode=stretch --loop"]
-	
-	#run all the commands on all the clients
+	cmds = ["~/dbuscontrol.sh stop", "sleep 2", "omxplayer /mnt/usb/media/intro.mp4 --aspect-mode=stretch --loop"]
+	print("Playing screen saver on all clients")
 	for cmd in cmds:
 		client.run_command(cmd, stop_on_errors=False)
+	
+def pause_movie(choice, clientID) :
+	command = "~/dbuscontrol.sh pause"
+	ssh_clients[clientID].exec_command(command)
+	print("Pausing " + choice + " movie on " + hosts[clientID] + "\r")	
 		
 def shutdown_all():
   cmds=["shutdown now"]
--- installation.py
+++ installation.py
@@ -5,6 +5,11 @@ select an item. Once selected, a message is sent to a remote client, telling it 
 that corresponds to the selection. After all the selections are made, the videos play for
 15 seconds and then the clients revert to a screen-saver video loop.
 """
+import ClientMoviePlayer as clientPlayer
+
+#start up client machines
+clientPlayer.play_screensaver()
+
 import pi3d
 import time
 import os
@@ -12,7 +17,10 @@ import RPi.GPIO as gpio
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import ScaleValues as scaleVal
-import ClientMoviePlayer as clientPlayer
+import RGBCompliment as rgbColor
+
+home_path = "/home/pi/naropa_installation/"
+
 
 gpio.setmode(gpio.BCM)
 gpio.setup(17, gpio.IN)
@@ -28,11 +36,11 @@ mcp= Adafruit_MCP3008.MCP3008(clk=CLK,cs=CS,miso=MISO,mosi=MOSI)
 DEBUG = False
 READKNOB = False
 
-#start up client machines
-clientPlayer.play_screensaver()
+randcolor = rgbColor.random_colorf(1.0)
 
 # Setup display and initialise pi3d
-DISPLAY = pi3d.Display.create(x=100, y=100, background=(0.0, 0.0, 0.0, .5), frames_per_second=30 )  
+DISPLAY = pi3d.Display.create(x=100, y=100, background=(0,0,0,.5), frames_per_second=60)  
+
 CAMERA = pi3d.Camera()
 CAMERA2D = pi3d.Camera(is_3d=False)
                 
@@ -41,80 +49,67 @@ light = pi3d.Light(lightpos=(-2.0, 3.0, 5.0), lightcol=(30.0, 30.0, 30.0), light
 flatsh = pi3d.Shader("uv_flat")
 shadermat = pi3d.Shader("mat_flat")
 
+xyz = .001
 
+gun = pi3d.Model(file_string= home_path + 'models/gun.obj', camera=CAMERA, name='gun',z=10.0)
+gun.scale(xyz,xyz,xyz)
 
-cow = pi3d.Model(file_string='models/cow2.obj', camera=CAMERA, name='gun',z=10.0)
-cow.scale(.5,.5,.5)
+bear = pi3d.Model(file_string=home_path + 'models/bear.obj', camera=CAMERA, name='bear', z=10.0)
+bear.scale(xyz,xyz,xyz)
 
-bear = pi3d.Model(file_string='models/bear.obj', camera=CAMERA, name='bear', z=10.0)
-bear.scale(.25,.25,.25)
+fork = pi3d.Model(file_string=home_path + 'models/fork.obj', camera=CAMERA, name='fork', z=10.0)
+fork.scale(xyz,xyz,xyz)
 
-skull = pi3d.Model(file_string='models/skull.obj', camera=CAMERA, name='fork', z=10.0)
-skull.scale(.5,.5,.5)
+knife = pi3d.Model(file_string=home_path + 'models/knife.obj', camera=CAMERA, name='knife', z=1.0)
+knife.scale(xyz,xyz,xyz)
 
-teapot = pi3d.Model(file_string='models/teapot.obj', camera=CAMERA, name='pipe', z=10.0)
-teapot.scale(.8,.8,.8)
+cake = pi3d.Model(file_string=home_path + 'models/cake.obj', camera=CAMERA, name='cake', z=10.0)
+cake.scale(xyz,xyz,xyz)
 
-plane = pi3d.Model(file_string='models/biplane.obj', camera=CAMERA, name='cake', z=10.0)
-plane.scale(.25,.25,.25)
-
-models = [bear,cow,skull,teapot,plane]
+models = [bear,gun,fork,cake,knife]
 
 for model in models :
 	#model.set_shader(shadermat)
-	model.set_line_width(line_width=1.0,strip=False,closed=False)
+	model.set_line_width(line_width=1.0,strip=False,closed=True)
 	
 labels = []
 
 #Generate the labels for the models
 for x in range(0,len(models)) :
-	strin = pi3d.FixedString('fonts/NotoSans-Regular.ttf', models[x].name, font_size=48, color=(70, 70, 180, 255), background_color=None,
+	strin = pi3d.FixedString(home_path + 'fonts/NotoSans-Regular.ttf', models[x].name, font_size=144, color=(255, 255, 255, 255), background_color=None,
 camera=CAMERA2D, shader=flatsh, f_type='SMOOTH')
-	strin.sprite.position(0,-250,2)
+	strin.sprite.position(0,0,2)
 	labels.append(strin)
 
 new_labels = labels[:] #copy the lists so we can regenerate later
 new_models = models[:]
 
-waitscreen = pi3d.FixedString('fonts/NotoSans-Regular.ttf', "Waiting for Godot", font_size=72, color=(255, 0, 0, 255), background_color=(255,255,255,255),
+waitscreen = pi3d.FixedString(home_path + 'fonts/NotoSans-Regular.ttf', "Waiting for a bit...", font_size=72, color=(255, 0, 0, 255), background_color=(255,255,255,255),
 camera=CAMERA2D, shader=flatsh, f_type='SMOOTH')
 waitscreen.sprite.position(0,0,0)
 
 selectionID = 0
 clientMachine = 0
 isPlayingVideoLoop = False
+showSelectedLabel = False
 
 def ReadInput() :
 	global selectionID
 	global READKNOB
-	global clientMachine
-	global models
-	global labels
-	global isPlayingVideoLoop
+	#global clientMachine
+	#global models
+	#global labels
+	global showSelectedLabel
 	
 	input_value = gpio.input(17)
 	input_value2 = gpio.input(18)
 	
 	if input_value == False:
-		#DISPLAY.set_background(1,1,1,1)
-		print('You selected a ', models[selectionID].name , ' client is ', clientMachine, '\r')
-		
+		showSelectedLabel = True
+		#print('You selected a ', models[selectionID].name , ' client is ', clientMachine, '\r')
+		DISPLAY.set_background(.5,.5,.5,1)
 		clientPlayer.open_movie(models[selectionID].name, clientMachine)
-		
-		models.remove(models[selectionID])
-		labels.remove(labels[selectionID])
-		
-		clientMachine += 1
-		
-		if clientMachine == 4 : #We are on the last choice
-			isPlayingVideoLoop = True
-			print('opening last choice on screen 5\r')
-			clientPlayer.open_movie(models[0].name, 4)
-			clientMachine = 0
-			models = new_models[:]
-			labels = new_labels[:]
-			DISPLAY.set_background(1,1,1,1)
-			
+		clientPlayer.pause_movie(models[selectionID].name, clientMachine)	
 		READKNOB = True
 		while input_value == False:
 			input_value = gpio.input(17)
@@ -148,6 +143,7 @@ intervalCount = 0
 # Fetch key presses
 mykeys = pi3d.Keyboard()
 waitTime =0
+labelWaitTime = 0
 
 while DISPLAY.loop_running():
 
@@ -155,23 +151,38 @@ while DISPLAY.loop_running():
 	if rotY >= 360 :
 		rotY = 0
 	
-	#rotX += .2
-	#if rotX >= 360  :
-		#rotX = 0
-	
 	intervalCount += .1
 	if intervalCount >= 1 :
 		intervalCount =0
 		READKNOB = True
-	
-	
-	
+
 	if isPlayingVideoLoop == False :
-		ReadInput()
-		models[selectionID].rotateToY(rotY)
-		models[selectionID].rotateToX(rotX)
-		models[selectionID].draw()
-		labels[selectionID].draw()
+		if showSelectedLabel == True :
+			labels[selectionID].draw()
+			labelWaitTime += 1
+			if labelWaitTime >= 120 :
+				showSelectedLabel = False
+				labels.remove(labels[selectionID])
+				models.remove(models[selectionID])
+				labelWaitTime = 0
+				DISPLAY.set_background(0,0,0,.5)
+				clientMachine += 1
+				if clientMachine == 4 : # we just played on the 4th client, only one left to go, so automatically progress now
+					showSelectedLabel = True
+					clientPlayer.open_movie(models[0].name, clientMachine)
+					selectionID =0
+					DISPLAY.set_background(.5,.5,.5,1)
+				elif clientMachine == 5 : #we finished with the last delay, show the wait screen
+					isPlayingVideoLoop = True
+					clientMachine = 0
+					models = new_models[:]
+					labels = new_labels[:]
+					DISPLAY.set_background(1,1,1,1)
+		else :
+			ReadInput()
+			models[selectionID].rotateToY(rotY)
+			models[selectionID].rotateToX(rotX)
+			models[selectionID].draw()
 	else :
 		waitscreen.draw()
 		waitTime += 1
